bc666bbf6903d9b284117698d3fbee76ea3182d7
BehrouzRajaei <bmd.rajaei@mail.de>
2026-02-24 02:51:49 +0100

    Complete portfolio implementation

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/header';
+import Hero from '../components/hero';
+import Skills from '../components/my-skills';
+import Projects from '../components/projects';
+import Contact from '../components/contact';
+import Footer from '../components/footer';
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
